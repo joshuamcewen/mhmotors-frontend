a18e0613927a761658e05be24822c2554e294362
Josh McEwen <joshua@joshuamcewen.co.uk>
2020-06-18 18:16:39 +0100

    Compose landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,45 +1,20 @@
 import React from 'react';
 import { useTheme } from 'react-jss';
 import useStyles from './App.styles';
-import { Footer, Hero, Map, ServiceCard, Socials } from '../../components';
+import { Footer, Hero, Map, Services, Socials } from '../../components';
 
 const App = () => {
     const theme = useTheme();
     const classes = useStyles({ theme });
 
     return (
-        <>
+        <div className={classes.app}>
             <Hero />
-            <div className={classes.container}>
-                <div className={classes.services}>
-                    <h2 className={classes.serviceHeader}>Our Services</h2>
-                    <div className={classes.serviceCards}>
-                        <ServiceCard title="Repairs">
-                            Our mechanics have a wealth of 30+ years experience
-                            in repairing vehicles of all makes and models. If
-                            you think there might something wrong with your
-                            vehicle, get in touch and we can discuss your
-                            options.
-                        </ServiceCard>
-                        <ServiceCard title="Servicing">
-                            We also offer servicing packages where we’ll give
-                            your vehicle a full health check spanning your
-                            lights, tyres, exhaust, brakes, transmission, oil
-                            and engine filters to name just a few of the
-                            components we look at.
-                        </ServiceCard>
-                        <ServiceCard title="MOTs">
-                            As an approved MOT station, we can check your
-                            vehicle for roadworthiness, safety and environmental
-                            compliance. Give us to book your vehicle in.
-                        </ServiceCard>
-                    </div>
-                </div>
-            </div>
+            <Services />
             <Map />
             <Socials />
             <Footer />
-        </>
+        </div>
     );
 };
 
--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -15,43 +15,5 @@ export default createUseStyles(({ breakpoints, colors }) => ({
         'h1, h2, h3': {
             fontWeight: '600'
         }
-    },
-    container: {
-        maxWidth: '60em',
-        margin: '0 auto',
-        display: 'flex',
-        flexDirection: 'column'
-    },
-    serviceHeader: {
-        display: 'none',
-
-        [breakpoints.sm]: {
-            display: 'block',
-            textAlign: 'center',
-            fontWeight: 700,
-            fontSize: '1.8em',
-            color: colors.headings,
-            margin: '1.5em 0 1em'
-        }
-    },
-    serviceCards: {
-        display: 'flex',
-        flexDirection: 'column',
-        margin: '-2em 1em 1em',
-
-        [breakpoints.sm]: {
-            margin: '1em 0',
-            alignItems: 'stretch',
-            flexDirection: 'row'
-        },
-
-        '& .ServiceCard': {
-            width: 'calc(100% - 4em)',
-            margin: '0.5em',
-
-            [breakpoints.sm]: {
-                width: '33.3333%'
-            }
-        }
     }
 }));
